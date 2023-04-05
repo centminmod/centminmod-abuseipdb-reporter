0ef7cb21a225abaa0de0a8d5439cfb87014a455b
George Liu (eva2000) <eva2000@centminmod.com>
2023-04-06 00:52:04 +1000

    add abuseipdb-plotter.py
    - use plotly to parse API JSON logs to provide HTML charts. HTML page has 2 charts but only chart 1 is properly working right now

---
+++ abuseipdb-plotter.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import json
+from collections import defaultdict
+from datetime import datetime
+import plotly.graph_objects as go
+import plotly.io as pio
+from plotly.io import to_json
+from datetime import datetime, timedelta
+
+# Load the JSON data from the log file
+with open('/var/log/abuseipdb-reporter-api-json.log', 'r') as f:
+    logs = json.load(f)
+
+# Prepare data structures for the charts
+ip_submissions = defaultdict(int)
+ip_scores = defaultdict(float)
+
+# Process the logs
+for log in logs:
+    ip = log['sentIP']
+    trigger = log.get('notsentTrigger', 'Unknown')
+    timestamp = log.get('sentTimestamp', None)
+    api_response = log.get('apiResponse', None)
+
+    if api_response:
+        try:
+            confidence_score = api_response['data'].get('abuseConfidenceScore', 0)
+            ip_scores[ip] += confidence_score
+            ip_submissions[ip] += 1
+        except KeyError:
+            pass
+
+# Generate chart 1
+top_ips = sorted(ip_scores.items(), key=lambda x: x[1], reverse=True)[:10]
+ip_addresses = [ip[0] for ip in top_ips]
+submission_counts = [ip_submissions[ip[0]] for ip in top_ips]
+confidence_scores = [ip[1] for ip in top_ips]
+
+fig1 = go.Figure(go.Bar(x=ip_addresses, y=submission_counts, name='IP Addresses'))
+fig1.update_layout(
+    title='Top 10 IP Addresses Submitted with Abuse Confidence Scores',
+    xaxis_title='IP Address',
+    yaxis_title='Total API Submissions'
+)
+
+# Generate chart 2
+# Get the current time
+now = datetime.now()
+
+# Calculate the time 24 hours ago
+last_24_hours = now - timedelta(hours=24)
+
+# Filter out the logs older than 24 hours
+recent_logs = [log for log in logs if log.get('apiResponse', {}).get('data', {}).get('abuseConfidenceScore', None) is not None and 'sentTimestamp' in log and datetime.strptime(log['sentTimestamp'], '%Y-%m-%dT%H:%M:%S.%fZ') >= last_24_hours]
+
+# Aggregate hourly submissions
+hourly_counts = defaultdict(lambda: defaultdict(int))
+
+for log in recent_logs:
+    ip = log['sentIP']
+    timestamp = datetime.strptime(log['sentTimestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    current_hour = timestamp.replace(minute=0, second=0, microsecond=0)
+
+    trigger = log.get('notsentTrigger', 'Unknown')
+    confidence_score = log.get('apiResponse', {}).get('data', {}).get('abuseConfidenceScore', 0)
+
+    if confidence_score > 0:
+        hourly_counts[current_hour]['total'] += 1
+        hourly_counts[current_hour][trigger] += 1
+
+hourly_timestamps = [last_24_hours + timedelta(hours=i) for i in range(24)]
+hourly_stacked_counts = []
+
+for trigger in set(t for hour in hourly_counts.values() for t in hour.keys()):
+    counts = [hourly_counts[hour].get(trigger, 0) for hour in hourly_timestamps]
+    hourly_stacked_counts.append(go.Bar(x=hourly_timestamps, y=counts, name=trigger))
+
+xaxis_tickvals = hourly_timestamps
+xaxis_ticktext = [hour.strftime('%Y-%m-%d %H:%M:%S') for hour in hourly_timestamps]
+
+fig2 = go.Figure(data=hourly_stacked_counts)
+fig2.update_layout(
+    title='Hourly Total IP Submissions with Abuse Confidence Scores in the Last 24 Hours',
+    xaxis_title='Hour',
+    yaxis_title='Submission Count',
+    barmode='stack',
+    xaxis_range=[last_24_hours, now],
+    xaxis_tickvals=xaxis_tickvals,
+    xaxis_ticktext=xaxis_ticktext
+)
+
+# Save chart 1 JSON data to a file
+with open('chart1_data.json', 'w') as chart1_file:
+    chart1_file.write(to_json(fig1))
+
+# Save chart 2 data to a file
+def serialize_datetime(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
+with open('chart2_data.json', 'w') as chart2_file:
+    json.dump(fig2.to_dict(), chart2_file, indent=2, default=serialize_datetime)
+
+# Load chart 2 data from file
+with open('chart2_data.json', 'r') as f:
+    chart2_data = json.load(f)
+
+# Create the HTML file with two chart containers and the Plotly.js library from a CDN
+html_template = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Charts</title>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+</head>
+<body>
+    <div id="chart1"></div>
+    <div id="chart2"></div>
+    <script>
+        Plotly.react('chart1', {0});
+        Plotly.react('chart2', {1});
+    </script>
+</body>
+</html>
+'''
+
+with open('charts.html', 'w') as f:
+    f.write(html_template.format(to_json(fig1), to_json(fig2)))
